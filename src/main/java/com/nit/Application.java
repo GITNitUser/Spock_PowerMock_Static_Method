328554c3237b830e638e75fb7fdf16f93e5dd597
ILYAS ABBASI <github880@gmail.com>
2020-12-03 00:20:55 +0530

    committing java static method groovy test cases

---
+++ src/main/java/com/nit/Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import java.io.IOException;
+import java.util.GregorianCalendar;
+
+public class Application {
+  public static void main(String[] args) throws IOException {
+    Person person = new Person("Kriegisch", "Alexander", new GregorianCalendar(1971, 5 - 1, 8).getTime());
+    final String fileName = "kriegisch.txt";
+    person.writeToFile(fileName);
+    System.out.println(person + " written to file '" + fileName + "'");
+  }
+}
